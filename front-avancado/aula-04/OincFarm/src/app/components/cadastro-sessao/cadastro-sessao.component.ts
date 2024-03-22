6afad29fb9b994d823e7a0b497325fd4dd998bf1
leogmc <ifbaianoleo@gmail.com>
2024-03-22 14:09:02 -0300

    Continuando atividade 4

--- front-avancado/aula-04/OincFarm/src/app/components/cadastro-sessao/cadastro-sessao.component.ts
+++ front-avancado/aula-04/OincFarm/src/app/components/cadastro-sessao/cadastro-sessao.component.ts
@@ -1,12 +1,77 @@
 import { Component } from '@angular/core';
 
+//@angular/material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButton } from '@angular/material/button';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Suino } from '../../models/suino.model';
+import { BancoService } from '../../utils/banco.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SessaoService } from '../../utils/sessao.service';
+
 @Component({
   selector: 'app-cadastro-sessao',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,MatInputModule,MatDatepickerModule,MatSelectModule, ReactiveFormsModule, CommonModule, MatNativeDateModule, MatButton],
   templateUrl: './cadastro-sessao.component.html',
   styleUrl: './cadastro-sessao.component.css'
 })
 export class CadastroSessaoComponent {
+
+  sessaoForm!: FormGroup;
+  suinos!: Suino[];
+  
+ constructor(private servico: BancoService,private formBuilder: FormBuilder, private sessaoService: SessaoService){}
+
+
+ ngOnInit(): void {
+  this.carregarSuinos();
+  this.sessaoForm = this.formBuilder.group({
+    data: ['', Validators.required],
+    descricao: ['', Validators.required],
+    brincos: [[]], // Inicializa com um array vazio
+    atividadesPlanejadas: [[]] // Inicializa com um array vazio
+   
+  });
+}
+
+  carregarSuinos() {
+    this.servico.getSuinos().subscribe(suinos => {
+      this.suinos = suinos;
+    });
+  }
+
+
+  onSubmit() {
+    if (this.sessaoForm.valid) {
+      const novaSessao = this.sessaoForm.value;
+      console.log('Nova Sessão:', novaSessao);
+      this.sessaoService.adicionarSessao(novaSessao);
+      this.sessaoForm.reset();
+    }
+  }
+
+  addAtividade() {
+    const atividadeSelecionada = this.sessaoForm.get('atividadesPlanejadas')?.value;
+    const atividadesArray = this.sessaoForm.get('atividadesPlanejadas');
+    if (atividadeSelecionada && atividadesArray) {
+      atividadesArray.setValue([...atividadesArray.value, atividadeSelecionada]);
+    }
+  }
+
+  addSuino() {
+    const suinoSelecionado = this.sessaoForm.get('brincos')?.value;
+    const brincosControl = this.sessaoForm.get('brincos');
+    if (suinoSelecionado && brincosControl) {
+      const brincosArray = [...brincosControl.value, suinoSelecionado];
+      brincosControl.setValue(brincosArray);
+    }
+  }
+  
 
 }
--- front-avancado/aula-04/OincFarm/src/app/components/menu-sessao/menu-sessao.component.ts
+++ front-avancado/aula-04/OincFarm/src/app/components/menu-sessao/menu-sessao.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu-sessao',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './menu-sessao.component.html',
   styleUrl: './menu-sessao.component.css'
 })
